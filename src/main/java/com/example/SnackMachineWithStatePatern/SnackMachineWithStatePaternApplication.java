77b5a7609fa192bd00543b241cc4f3efc9bdecbe
Marcin Krukowski <marcin.krukowski.90@wp.pl>
2023-03-23 18:26:03 +0100

    feat: project start

---
+++ src/main/java/com/example/SnackMachineWithStatePatern/SnackMachineWithStatePaternApplication.java
@@ -0,0 +1,13 @@
+package com.example.SnackMachineWithStatePatern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnackMachineWithStatePaternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnackMachineWithStatePaternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SnackMachineWithStatePatern/SnackMachineWithStatePaternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SnackMachineWithStatePatern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnackMachineWithStatePaternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
